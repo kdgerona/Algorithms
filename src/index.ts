fce45b516cc8d70cd2cdd176de2b50d306b36918
Kevin Dave Gerona <kgerona@dnamicro.com>
2022-08-26 04:36:14 +0800

    Added: bubble sort

--- src/index.ts
+++ src/index.ts
@@ -168,148 +168,70 @@ export class LinkedList<T = any> {
     return;
   }
 
-  sort<P = any>(): Array<P> {
+  sort<P = any>(): Array<T> {
     if (this.head === null) return [];
 
-    // let bubble = this.head;
-    // let operation
-
-    // do {
-    //     if(!bubble) return []
-
-    //     const curr_bubble = bubble
-    //     const bubble_right = bubble.right
-
-    //     if(!bubble_right) {
-    //         // @ts-ignore
-    //         // bubble = curr_bubble
-
-    //         // return []
-
-    //         return
-    //     }
-
-    //     if(curr_bubble.value > bubble_right?.value) {
-    //         if(curr_bubble.left === null){
-    //             //@ts-ignore
-    //             bubble_right?.left = nullhead_pointer = head_pointer.right;
-
-    //     bubble = bubble_right
-
-    //     // console.log('IM RIGHT', test)
-    // } while(bubble.right !== null)
-
-    // return []
-
-    // DEV
     let temp_head = this.head;
     let head_pointer = this.head;
-    // let process_head;
+    let should_execute_swap_operation = true;
+    let swap_operation_count = 0;
 
     do {
       const next_pointer = head_pointer.right as ListNode<T>;
 
-      this.head = temp_head as ListNode<T>;
-  
-      const list = this.getAll();
+      if (!next_pointer) {
+        console.log(`Swap Operation Count - ${swap_operation_count}`)
+        if (swap_operation_count !== 0) {
+          console.log('Reevaluating...');
+          head_pointer = temp_head;
+          swap_operation_count = 0;
+          should_execute_swap_operation = true;
 
-      console.log('TEST123', list)
+          continue;
+        } else {
+          should_execute_swap_operation = false;
+          break;
+        }
+      }
 
-      // Some logic here
-      // Is head
-      //   if (!head_pointer.left === null) {
-      //     if (head_pointer.value > next_pointer?.value) {
-      //       const [anode, bnode] = this.swap(head_pointer, next_pointer);
-      //       temp_head = anode;
-      //       head_pointer = bnode;
-      //     }
-      //   }
-
-    //   10 > 50
-        console.log('Evaluate', head_pointer.value, next_pointer?.value)
+      console.log(
+        'Evaluating...',
+        head_pointer.value,
+        next_pointer?.value ?? null
+      );
       if (head_pointer.value > next_pointer?.value) {
-        const [anode, bnode] = this.swap(next_pointer, head_pointer);
-        // const [anode, bnode] = this.swap(head_pointer, next_pointer);
+        const swapped_node = this.swap(head_pointer, next_pointer);
+        swap_operation_count += 1;
 
+        if (!swapped_node.left) temp_head = swapped_node;
 
-        if (!head_pointer.left) temp_head = anode;
-
-        head_pointer = bnode.right as ListNode<T>;;
+        head_pointer = swapped_node.right as ListNode<T>;
       } else {
         head_pointer = next_pointer;
       }
+    } while (head_pointer !== null && should_execute_swap_operation);
 
-      //   head_pointer = head_pointer.right as ListNode<T>;
-    } while (head_pointer !== null);
+    this.head = temp_head;
+    const getList = this.getAll();
 
-    // console.log("TEST",)
-
-    // @ts-ignore
-    return list;
-    // END
+    return getList;
   }
 
-  // Swap a node to b node
-  swap(anode: ListNode<T>, bnode: ListNode<T>): [ListNode<T>, ListNode<T>] {
-    const swapped_anode: ListNode<T> = {
-      left: bnode.left,
-      right: bnode.right,
-      value: bnode.value,
-    };
+  swap(anode: ListNode<T>, bnode: ListNode<T>): ListNode<T> {
+    const clonedNode = { ...anode };
 
-    const swapped_bnode: ListNode<T> = {
-      left: anode.left,
-      right: anode.right,
-      value: anode.value,
-    };
+    anode.value = bnode.value;
+    bnode.value = clonedNode.value;
 
-    return [swapped_anode, swapped_bnode];
+    return anode;
   }
 }
 
 const list = new LinkedList<number>();
-
-// list.push(10);
-// list.push(20);
-// list.push(40);
-// list.push(50);
-
-// console.log(`Items`, list.getAll());
-// console.log(`At 0 `, list.get(0));
-// console.log(`At 1 `, list.get(1));
-// console.log(`At 3 `, list.get(3));
-// console.log(`Remove 2 `, list.remove(2));
-// console.log(`Items`, list.getAll());
-// console.log(`Shift Once`, list.shift());
-// console.log(`Shift Twice`, list.shift());
-// console.log(`Items`, list.getAll());
-// console.log(`Push 10 `, list.push(10));
-// console.log(`Push 20 `, list.push(20));
-// console.log(`Push 30 `, list.push(30));
-// console.log(`Items`, list.getAll());
-// console.log(`Pop Once`, list.pop());
-// console.log(`Items`, list.getAll());
-
-// console.log(`Splice 100 at 1`, list.splice(100, 1));
-// console.log(`Items`, list.getAll());
-// console.log(`Pop Once`, list.pop());
-// console.log(`Items`, list.getAll());
-// console.log(`Pop Once`, list.pop());
-// console.log(`Items`, list.getAll());
-// console.log(`Pop Once`, list.pop());
-// console.log(`Items`, list.getAll());
-// console.log(`Push 30 `, list.push(30));
-// console.log(`Items`, list.getAll());
-// console.log(`Shift Once`, list.shift());
-// console.log(`Items`, list.getAll());
-// console.log(`Shift Once`, list.shift());
-
-list.push(10);
 list.push(50);
+list.push(10);
 list.push(40);
 list.push(20);
 
 console.log(`Items`, list.getAll());
-
-// list.sort();
 console.log(`Sorted Items`, list.sort());
